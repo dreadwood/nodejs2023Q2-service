6689fd224188e9924f3ffdc03606f09088d6712b
srg <skabassjob@gmail.com>
2023-07-31 23:42:41 +0400

    feat: connect dotenv and set default port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { DEFAULT_PORT } from './const';
+
+dotenv.config();
+const port = process.env.PORT ?? DEFAULT_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
+
+  console.log(`Listen port: ${port}`);
 }
 bootstrap();
