c241d635b8dced6bc014d18bde1539cfa3bc246b
srg <skabassjob@gmail.com>
2023-08-05 22:39:26 +0400

    feat: set and set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { DEFAULT_PORT } from './const';
+import * as yaml from 'js-yaml';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { DEFAULT_PORT } from './const';
 
 dotenv.config();
 const port = process.env.PORT ?? DEFAULT_PORT;
@@ -10,6 +14,12 @@ const port = process.env.PORT ?? DEFAULT_PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const resolvedPath = join('doc', 'api.yaml');
+  const apiDoc = await readFile(resolvedPath, { encoding: 'utf8' });
+  const apiDocObj = yaml.load(apiDoc);
+
+  SwaggerModule.setup('api', app, apiDocObj as OpenAPIObject);
 
   await app.listen(port, () => console.log(`Listen port: ${port}`));
 }
